7202b102ccea5a9c17a213edadd75fcad6052fec
Pouya <titanpooyanote@gmail.com>
2025-02-23 18:57:21 +0330

    initial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import CounterConfig from './components/Counter/CounterConfig.jsx';
+
+function App() {
+  log('<App /> rendered');
+
+  const [chosenCount, setChosenCount] = useState(0);
+  function handleSetCount (){
+    setChosenCount(newCount)
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <CounterConfig onSet={handleSetCount}/>
+        <Counter initialCount={chosenCount} />
+      </main>
+    </>
+  );
+}
+
+export default App;
